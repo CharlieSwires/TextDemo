79c784cb08350cc0b4d675f518b5a71e28669a0f
CharlieSwires <charlieswires@gmail.com>
2019-04-19 00:16:36 +0100

    bugs fixed

--- src/TextDemoSource.java
+++ src/TextDemoSource.java
@@ -1,36 +1,4 @@
-/*
- * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- *   - Neither the name of Oracle or the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */ 
 
-
-/* TextDemo.java requires no other files. */
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -54,10 +22,10 @@ public class TextDemoSource extends JPanel implements ActionListener {
 	protected static JTextField textField;
 	protected static JTextField textField2;
 	protected static JTextArea textArea;
-	static String nodeName = "1";
-	static JFrame frame;
+	String nodeName = "1";
+	static TextDemoSource tds = null;
+	
 	private final static String newline = "\n";
-	static TextDemoSource td;
 
 	public TextDemoSource() {
 		super(new GridBagLayout());
@@ -89,8 +57,21 @@ public class TextDemoSource extends JPanel implements ActionListener {
 	public void actionPerformed(ActionEvent evt) {
 		if (evt.getSource()==textField){
 			textField.selectAll();
+			Track track = new Track();
+			track.setDest(nodeName);
+			track.setText(textField.getText());
 			try {
-				sendPost();
+				new Thread() {
+					public void run() {
+						try {
+							sendPost(track);
+						} catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}.start();
+				
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -98,14 +79,12 @@ public class TextDemoSource extends JPanel implements ActionListener {
 			if(textField.getText() != null){
 				textArea.append(textField2.getText()+": "+textField.getText() + newline);
 			}
-	        textArea.setCaretPosition(textArea.getDocument().getLength());
-			System.out.println("repaint()");
 			//Make sure the new text is visible, even if there
 			//was a selection in the text area.
 			textArea.setCaretPosition(textArea.getDocument().getLength());
+			System.out.println("repaint()");
 		} else if (evt.getSource()==textField2){
 			nodeName = textField2.getText();
-			frame.setTitle(nodeName);
 		}
 	}
 
@@ -115,12 +94,13 @@ public class TextDemoSource extends JPanel implements ActionListener {
 	 * event dispatch thread.
 	 */
 	private static void createAndShowGUI() {
+		JFrame frame = new JFrame("Source");
 		//Create and set up the window.
-		frame = new JFrame("Source");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		//Add contents to the window.
-		frame.add(new TextDemoSource());
+		tds = new TextDemoSource();
+		frame.add(tds);
 
 		//Display the window.
 		frame.pack();
@@ -128,24 +108,22 @@ public class TextDemoSource extends JPanel implements ActionListener {
 	}
 
 	// HTTP POST request
-	private static void sendPost() throws Exception {
+	private static void sendPost(Track track2) throws Exception {
 
 		String url = "http://localhost:8080/test-web-service/central/post";
 		URL obj = new URL(url);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		System.out.println("\nSending 'POST' request to URL : " + url);
 
 		//add reuqest header
 		con.setRequestMethod("POST");
 		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
 		con.setRequestProperty( "Content-Type", "application/json"); 
 		con.setRequestProperty( "charset", "utf-8");
-
+		con.setConnectTimeout(1000);
 		ObjectMapper mapper = new ObjectMapper();
 		//JSON from String to Object
-		Track track = new Track();
-		track.setDest(nodeName);
-		track.setText(textField.getText());
-		String outputPayload = mapper.writeValueAsString(track);
+		String outputPayload = mapper.writeValueAsString(track2);
 		con.setRequestProperty( "Content-Length", Integer.toString( outputPayload.getBytes().length ));
 
 		// Send post request
@@ -155,10 +133,6 @@ public class TextDemoSource extends JPanel implements ActionListener {
 		wr.flush();
 		wr.close();
 
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Post parameters : " + outputPayload);
-		System.out.println("Response Code : " + responseCode);
 
 		BufferedReader in = new BufferedReader(
 				new InputStreamReader(con.getInputStream()));
@@ -172,6 +146,10 @@ public class TextDemoSource extends JPanel implements ActionListener {
 
 		//print result
 		System.out.println(response.toString());
+		
+		int responseCode = con.getResponseCode();
+		System.out.println("Post parameters : " + outputPayload);
+		System.out.println("Response Code : " + responseCode);
 
 	}
 
@@ -180,8 +158,8 @@ public class TextDemoSource extends JPanel implements ActionListener {
 		//Schedule a job for the event dispatch thread:
 		//creating and showing this application's GUI.
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			
 			public void run() {
-				td = new TextDemoSource();
 				createAndShowGUI();
 			}
 		});
